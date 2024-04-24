64996590c47f7c6eb3dba027d0ceaddabff7db46
Sharear Ahammed <sharearahammed@gmail.com>
2024-04-24 11:33:32 +0600

    second commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC6HD-7_YRJa_fB8RDXMRKXgAhvj9_C6L8",
-  authDomain: "coffee-store-b48e3.firebaseapp.com",
-  projectId: "coffee-store-b48e3",
-  storageBucket: "coffee-store-b48e3.appspot.com",
-  messagingSenderId: "372877541649",
-  appId: "1:372877541649:web:6c7366e82bff13a3feccb0"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
